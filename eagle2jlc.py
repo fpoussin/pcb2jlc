16cdcc122ec2ed91362a5e17549cd72c464e769f
Fabien Poussin <fabien.poussin@gmail.com>
2020-03-19 22:13:44 +0100

    Adding support for LCSC# custom attribute

--- eagle2jlc.py
+++ eagle2jlc.py
@@ -4,7 +4,7 @@
 from os.path import dirname, abspath
 from argparse import ArgumentParser
 import xml.etree.ElementTree as ET
-import xlsxwriter
+import xlsxwriter, xlrd
 import requests
 import re
 import json
@@ -14,8 +14,10 @@ parser = ArgumentParser(description='Generate JLCPCB bom and cpl files from an e
 parser.add_argument('project', type=str, help='Eagle board file')
 parser.add_argument('-u', '--update', action='store_true', help='Update JLCPCB component cache')
 parser.add_argument('-o', '--online', action='store_true', help='Query JLCPCB for each component (cache not used)')
+parser.add_argument('-m', '--match', action='store_true', help='Only use LCSC# attribute')
 
 API = 'https://jlcpcb.com/shoppingCart/smtGood/selectSmtComponentList'
+DB_URL = 'https://jlcpcb.com/componentSearch/uploadComponentInfo'
 
 if __name__ == '__main__':
   
@@ -28,22 +30,32 @@ if __name__ == '__main__':
   jlc_compos = []
   
   if args.update and not args.online:
-    # Download a list of all components  
-    step = 100
-    for i in range(500):
-      print('{0}'.format(i*step))
-      r = requests.post(API, data={'currentPage': i, 'pageSize': step})
-      if r.status_code == 200:
-        try:
-          data = r.json()['data']['list']
-          data_len = len(data)
-          jlc_compos += data
-          if data_len < step:
-            break
-        except Exception as e:
-          print(e)
-          print(r.text)
-      
+    print('Downloading components list...')
+    r = requests.get(DB_URL)
+    if r.status_code == 200:
+      wb = xlrd.open_workbook(file_contents=r.content)
+      sheet = wb.sheet_by_index(0)
+      for i in range(sheet.nrows)[1:]:
+        r = sheet.row(i)
+        pn = r[0].value
+        mfrn = r[1].value
+        cat = r[2].value
+        scat = r[3].value
+        package = r[4].value
+        mfr = r[6].value
+        ltype = r[7].value
+        jlc_compos.append(
+          {'componentCode': pn,
+           'describe': cat,
+           'componentTypeEn': cat,
+           'componentLibraryType': ltype,
+           'stockCount': 1000, # No stock count in offline list
+           'componentSpecificationEn': package,
+           'componentBrandEn': mfr,
+           'componentModelEn': mfrn
+        })
+      print('{} components added'.format(len(jlc_compos)))
+        
     with open("jlcdb.txt", 'w') as f:
       json.dump(jlc_compos, f, indent=2)
   elif not args.online:
@@ -56,12 +68,16 @@ if __name__ == '__main__':
   
   for component in board.iter('element'):
     
-    value = component.attrib['value']
-    name = component.attrib['name']
-    package = component.attrib['package']
+    value = component.attrib['value'].strip().upper()
+    name = component.attrib['name'].strip().upper()
+    package = component.attrib['package'].strip().upper()
+    lcsc_prop = component.find(".//attribute[@name='LCSC#']")
+    lcsc_pn = ''
+    if lcsc_prop != None:
+      lcsc_pn = lcsc_prop.attrib.get('value', '').strip().upper()
     pos = (component.attrib['x'], component.attrib['y'])
     layer = 'Top'
-    index = '{0}/{1}'.format(value, package)
+    index = '{0}/{1}/{2}'.format(value, package, lcsc_pn)
     
     rot = component.attrib.get('rot', 'R0')
     if rot.startswith('MR'):
@@ -73,63 +89,95 @@ if __name__ == '__main__':
       continue
     
     if index not in compos:
-      compos[index] = {'parts': [], 'jlc': {}}
+      compos[index] = {'parts': [], 'jlc': 
+      {'desc': '', 'basic': False, 'code': '', 'package': '', 'partName': ''}}
     compos[index]['parts'].append((name, layer, pos, rot))
     
 # Part numbers
   for c, v in compos.items():
-    value = ' '+ c.split('/')[0]
-    package = c.split('/')[1]
+    value = c.split('/')[0].upper()
+    package = c.split('/')[1].upper()
+    lcscpn = c.split('/')[2].upper()
     desc = ''
-    # Trim R/C
-    if re.search(r'^C\d{4,5}', package, re.M): 
+    # Trim R/C/L
+    if re.search(r'^C\d{4,5}', package, re.M):
       package = package[1:]
       desc = 'CAPACITOR'
-    elif re.search(r'^R\d{4,5}', package, re.M): 
+    elif re.search(r'^R\d{4,5}', package, re.M):
       package = package[1:]
       desc = 'RESISTOR'
-    elif re.search(r'^L\d{4,5}', package, re.M): 
+    elif re.search(r'^L\d{4,5}', package, re.M):
       package = package[1:]
       desc = 'INDUCTOR'
     else:
-      # Ignore other packages as they are too specific
-      package = ''
-      
-    keyword = '{0} {1} {2}'.format(package.upper(), desc.upper(), value.upper())
-    if not keyword.strip():
-      continue
+      package = '' # Ignore most packages as they are too specific, see below
+      m = re.search(r'^.*LED.*\d{4,5}', package, re.M)
+      if m:
+        package = m.group(1)
+        desc = 'LED'
 
     names = []
     for n in v['parts']:
         names.append(n[0])
 
     if args.online:
+      if lcscpn:
+        keyword = lcscpn
+      else:
+        keyword = ''
+        if package:
+          keyword += '{} '.format(package)
+        if desc:
+          keyword += '{} '.format(desc)
+        keyword += '{}'.format(value)
+      keyword = keyword.strip()
+
+      if not keyword.strip():
+        continue
+
       post_data = {'keyword': keyword, 'currentPage': '1', 'pageSize': '40'}
       r = requests.post(API, data=post_data)
       jlc_compos = r.json()['data']['list']
+
+    found = False
     for entry in jlc_compos:
-      if desc == 'RESISTOR' and value.endswith('R'):
-        value = value.replace('R', 'OHM')
-      elif desc == 'RESISTOR':
-        value = value.replace('R', '.')
-      if entry['stockCount'] < len(names): # Ignore if the required quantity isn't available
-        continue
-      if desc and desc.upper() not in entry['describe'].upper():
-        continue
-      if package and package.upper() not in entry['describe'].upper() and package.upper() not in entry['componentSpecificationEn'].upper():
-        continue
-      if value.upper() not in entry['describe'].upper() and value.upper().strip() not in entry['componentModelEn'].upper():
-        continue
-        
-      v['jlc']['desc'] = entry['describe']
-      v['jlc']['code'] = entry['componentCode']
-      v['jlc']['basic'] = entry['componentLibraryType'] == 'base'
-      v['jlc']['package'] = entry['componentSpecificationEn']
-      v['jlc']['partNumber'] = entry['componentModelEn']
-      if v['jlc']['basic']:
+      # Check part number (LCSC# property) before anything else
+      if lcscpn == entry['componentCode']:
+        v['jlc']['desc'] = entry['describe']
+        v['jlc']['code'] = entry['componentCode']
+        v['jlc']['basic'] = entry['componentLibraryType'] == 'base'
+        v['jlc']['package'] = entry['componentSpecificationEn']
+        v['jlc']['partName'] = entry['componentModelEn']
+        found = True
         break
 
-    print(names, keyword, v['jlc'])
+    # Skip the rest if we are in strict matching mode or already found it using part code
+    if not args.match and not found:
+      for entry in jlc_compos:
+
+        if desc == 'RESISTOR' and value.endswith('R'):
+          value = value.replace('R', 'OHM')
+        elif desc == 'RESISTOR':
+          value = value.replace('R', '.')
+        if entry['stockCount'] < len(names): # Ignore if the required quantity isn't available
+          continue
+        if desc and desc not in entry['describe'].upper():
+          continue
+        if package and package not in entry['describe'] and package != entry['componentSpecificationEn'].upper():
+          continue
+        if value.upper() not in entry['describe'].upper() \
+        and value.upper().strip() not in entry['componentModelEn'].upper():
+          continue
+          
+        v['jlc']['desc'] = entry['describe']
+        v['jlc']['code'] = entry['componentCode']
+        v['jlc']['basic'] = entry['componentLibraryType'] == 'base'
+        v['jlc']['package'] = entry['componentSpecificationEn']
+        v['jlc']['partName'] = entry['componentModelEn']
+        if v['jlc']['basic']: # We have found a matching "basic" part, skip the rest
+          break
+
+    print(names, v['jlc'])
     
 # BOM
     
